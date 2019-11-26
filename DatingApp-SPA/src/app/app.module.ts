e9251a27bfe0aeb224e3f3896903a7d275e0b3b5
McCauley <Stephen.McCauley@allenovery.com>
2019-11-26 17:43:47 +0000

    Initial Commit

---
+++ DatingApp-SPA/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ValueComponent } from './value/value.component';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      ValueComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule // this was added in for api calls and imported above
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
